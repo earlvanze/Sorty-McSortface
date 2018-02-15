b4a23277e64fdf96464429c858569afbba797eb1
Earl Co <earlvanze@gmail.com>
2018-02-15 06:34:32 -0500

    minor updates. FPS 0.55

--- sorty-pi/utils/app_utils.py
+++ sorty-pi/utils/app_utils.py
@@ -1,4 +1,7 @@
 # From http://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
+# From https://www.pyimagesearch.com/2016/01/04/unifying-picamera-and-cv2-videocapture-into-a-single-class-with-opencv/
+# From https://www.pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
+# From https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
 
 import struct
 import six
@@ -73,6 +76,90 @@ class WebcamVideoStream:
         # return the frame most recently read
         return self.frame
 
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
+
+# Unifying picamera and cv2.VideoCapture into a single class with OpenCVPython
+class VideoStream:
+    def __init__(self, src=0, usePiCamera=False, resolution=(640, 480),
+        framerate=32):
+        # check to see if the picamera module should be used
+        if usePiCamera:
+            # only import the picamera packages unless we are
+            # explicity told to do so -- this helps remove the
+            # requirement of `picamera[array]` from desktops or
+            # laptops that still want to use the `imutils` package
+            from imutils.video.pivideostream import PiVideoStream
+ 
+            # initialize the picamera stream and allow the camera
+            # sensor to warmup
+            self.stream = PiVideoStream(resolution=resolution,
+                framerate=framerate)
+ 
+        # otherwise, we are using OpenCV so initialize the webcam
+        # stream
+        else:
+            self.stream = WebcamVideoStream(src=src)
+
+    def start(self):
+        # start the threaded video stream
+        return self.stream.start()
+ 
+    def update(self):
+        # grab the next frame from the stream
+        self.stream.update()
+ 
+    def read(self):
+        # return the current frame
+        return self.stream.read()
+ 
+    def stop(self):
+        # stop the thread and release any resources
+        self.stream.stop()
+
+
+class PiVideoStream:
+    def __init__(self, resolution=(640, 480), framerate=32):
+        # initialize the camera and stream
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture,
+            format="bgr", use_video_port=True)
+ 
+        # initialize the frame and the variable used to indicate
+        # if the thread should be stopped
+        self.frame = None
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+ 
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        for f in self.stream:
+            # grab the frame from the stream and clear the stream in
+            # preparation for the next frame
+            self.frame = f.array
+            self.rawCapture.truncate(0)
+ 
+            # if the thread indicator variable is set, stop the thread
+            # and resource camera resources
+            if self.stopped:
+                self.stream.close()
+                self.rawCapture.close()
+                self.camera.close()
+                return
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+ 
     def stop(self):
         # indicate that the thread should be stopped
         self.stopped = True
