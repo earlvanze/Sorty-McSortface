69030755fd2a5585c8e77dd44358779f7fbb82fe
Kevin Nguyen <kvn219@nyu.edu>
2018-02-20 12:29:55 -0500

    add inception to model downloads

--- sorty-pi/model/download.sh
+++ sorty-pi/model/download.sh
@@ -1,3 +1,9 @@
+# MobileNets
 wget -O ssd_mobilenet_v1_coco_2017_11_17.tar.gz http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2017_11_17.tar.gz
 tar -xzvf ssd_mobilenet_v1_coco_2017_11_17.tar.gz
 rm -rf ssd_mobilenet_v1_coco_2017_11_17.tar.gz
+
+# SSD Inception
+wget -O ssd_inception_v2_coco_2017_11_17.tar.gz http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2017_11_17.tar.gz
+tar -xzvf ssd_inception_v2_coco_2017_11_17.tar.gz
+rm -rf ssd_inception_v2_coco_2017_11_17.tar.gz
